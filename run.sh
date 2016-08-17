f867fe4aadd3fceebe42be3d2a85341efaf9b844
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-08-17 01:46:24 -0400

    Update user infomration of running ghp-import.

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@ info "Installing ghp-import."
 pip install --upgrade ghp-import
 
 info "Importing a document."
+git config user.email "pleasemailus@wercker.com"
+git config user.name "werckerbot"
 ghp-import $WERCKER_GHP_IMPORT_MSG -b $WERCKER_GHP_IMPORT_BRANCH $WERCKER_GHP_IMPORT_BASEDIR
 
 REPOSITORY=${WERCKER_GIT_OWNER}/${WERCKER_GIT_REPOSITORY}
 info "Pushing to $REPOSITORY."
 
 URL="https://$WERCKER_GHP_IMPORT_TOKEN@github.com/$REPOSITORY.git"
-git config user.email "pleasemailus@wercker.com"
-git config user.name "werckerbot"
 git push -f "$URL" $WERCKER_GHP_IMPORT_BRANCH 2>/dev/null
